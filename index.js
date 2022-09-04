6fc9fe1a5bc912b50f899d0545fb74ae24f79e6a
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-09-04 19:57:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //iterate through each number in the array
+  for(let i = 0; i < array.length; i++){
+    //for the current number identify a complement that adds to the target(comp = target - num)
+    // n steps
+    const complement = target - array[i]
+    //iterate through the rest of the array
+    for(let j = i + 1; j < array.length; j++){
+      //check if any number is our complement
+      //if so return true
+      // n*n steps
+      if(array[j]=== complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Runtime: O(n^2) 
+  space: O(n)
 */
 
 /* 
   Add your pseudocode here
+  hasTargetSum([22,29,4,6,30],25)
+  iterate through each number in the array
+  for the current number identify a complement that adds to the target(comp = target - num)
+  iterate through the rest of the array
+  check if any number is our complement
+  if so return true
+
+  if I reach the end of the array, return false.
 */
 
 /*
